f172f86b9c4ad8cf382229c9b8364389a035d820
mjflaherty <90433176+mjflaherty@users.noreply.github.com>
2021-11-18 15:10:50 -0500

    Update christmasTree.js
    Added Christmas tree with a mouseClicked function

--- christmasTree.js
+++ christmasTree.js
@@ -1,2 +1,12 @@
 //paste christmas tree code in here once completed with 1 task
-//hellooooo
+
+fill(37, 82, 0);
+noStroke();
+triangle(94,150,257,150,175,70);
+triangle(57,200,295,200,175,106);
+
+// mouseClicked = function () 
+// {
+//     image(getImage("avatars/spunky-sam-red"),mouseX - 10,mouseY -10,30,30);
+// };
+
