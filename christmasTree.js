378a63051cb1b1e9a73fd674db7b4e863d9276d3
Claudia Deverdits <88414158+claudiadeverdits@users.noreply.github.com>
2021-11-21 16:51:32 -0500

    Update christmasTree.js
    i fixed up the tree and turned it into a function, made an array of most of the ornaments we want to use, and made it so that the mouse clicked function puts a random ornament on the tree by making the index a random number from 0 to the length of the array

--- christmasTree.js
+++ christmasTree.js
@@ -1,12 +1,37 @@
 //paste christmas tree code in here once completed with 1 task
 
-fill(37, 82, 0);
+background(209, 209, 209);
+
+//function to draw christmas tree (no need for parameters yet bc tree doesn't need to ever be moved)
+var christmasTree = function(){
 noStroke();
-triangle(94,150,257,150,175,70);
-triangle(57,200,295,200,175,106);
+fill(77, 73, 66);
+rect(170, 264, 64, 100);
+fill(37, 82, 0);
+triangle(90, 160, 310, 160, 200, 45);
+triangle(50, 240, 350, 240, 200, 75);
+triangle(25, 320, 375, 320, 200, 130);
+image(getImage("space/star"), 153, -17);
+};
 
-// mouseClicked = function () 
-// {
-//     image(getImage("avatars/spunky-sam-red"),mouseX - 10,mouseY -10,30,30);
-// };
+//call the function to make it draw
+christmasTree();
 
+//array of ornaments
+var ornaments = [
+    getImage("avatars/spunky-sam"),
+    getImage("avatars/spunky-sam-green"),
+    getImage("avatars/spunky-sam-orange"),
+    getImage("avatars/spunky-sam-red"),
+    getImage("cute/GemBlue"),
+    getImage("cute/GemGreen"),
+    getImage("cute/GemOrange"), 
+    getImage("avatars/purple-pi"), 
+    getImage("avatars/purple-pi-pink"), 
+    getImage("avatars/purple-pi-teal")
+    ];
+
+mouseClicked = function (){
+    //every time mouse is clicked, random image from ornaments array is put on the screen
+    image(ornaments[round(random(0,ornaments.length))],mouseX - 10, mouseY - 10, 30, 30);
+};
